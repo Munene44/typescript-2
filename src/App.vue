41cad727ed05e5811f5ed9b1b24169f5d368f0f0
Munene44 <karvinmitchar@gmail.com>
2023-04-05 08:14:20 +0300

    styling

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css">
-
-  <div>
+  <div class="container mt-3">
     <form @submit.prevent="add">
-      <input v-model="todo" />
-      <button type="submit">Add</button>
+      <div class="input-group">
+        <input type="text" class="form-control" placeholder="Add a new todo item" v-model="todo">
+        <button type="submit" class="btn btn-primary">Add</button>
+      </div>
     </form>
-    <div v-for="(todo, index) of todos" :key="todo.id">
-      <input type="checkbox" v-model="todo.completed" />
-      <span :class="{ 'completed': todo.completed }">{{ todo.todo }}</span>
-      <button @click="deleteTodo(index)">delete</button>
+    <div class="list-group mt-3">
+      <div v-for="(todo, index) of todos" :key="todo.id" class="list-group-item">
+        <div class="form-check">
+          <input type="checkbox" class="form-check-input" v-model="todo.completed">
+          <label class="form-check-label" :class="{ 'text-muted': todo.completed }">{{ todo.todo }}</label>
+        </div>
+        <button type="button" class="btn btn-danger btn-sm" @click="deleteTodo(index)">Delete</button>
+      </div>
     </div>
   </div>
 </template>
-
 <script>
 import { v4 as uuidv4 } from "uuid";
 export default {
